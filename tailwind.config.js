cc8ea737e59d547b4a3e3387da17df8f0fbfafe8
Roy Mosby <ego.madking@gmail.com>
2022-04-08 18:05:04 -0400

    extends styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,30 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}"
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        cover: `3.3px 4.1px 3.2px rgba(0, 0, 0, 0.087),
+          9.8px 12.3px 10.7px rgba(0, 0, 0, 0.1),
+          31px 39px 48px rgba(0, 0, 0, 0.21)`,
+        'cover-divider': `4.6px 6.3px 6px rgba(0, 0, 0, 0.04),
+          24px 33px 48px rgba(0, 0, 0, 0.1)`,
+      },
+      fontFamily: {
+        display: 'Rye-Regular',
+      },
+      gridTemplateColumns: {
+        form: 'auto auto',
+      },
+      gridTemplateRows: {
+        cover: 'auto 1fr auto',
+      },
+      width: {
+        cover: 'calc(100vw - 2rem)',
+      },
+      minHeight: {
+        cover: 'calc(100vh - 5rem)',
+      },
+    },
   },
   plugins: [],
 }
