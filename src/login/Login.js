49415ee8f3d6649d4a984944984e689472c1eb34
Roy Mosby <ego.madking@gmail.com>
2022-04-11 12:39:36 -0400

    form wip

--- src/login/Login.js
+++ src/login/Login.js
@@ -1,7 +1,15 @@
+import { useState } from 'react'
 import logo from '../wrangler.svg'
 import { Button } from '../common/buttons/Button'
 
 export function Login() {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault()
+    console.log(username, password)
+  }
   return (
     <div className="border-4 border-slate-400 p-2">
       <div className="w-60 self-center m-auto">
@@ -9,12 +17,30 @@ export function Login() {
       </div>
       <div>
         <h2 className="font-display mb-2 text-2xl">Log in to Job Application Wrangler</h2>
-        <form action="" className="grid grid-cols-form gap-1 mb-4">
+        <form onSubmit={handleSubmit} className="grid grid-cols-form gap-1 mb-4">
           <label htmlFor="username">Username</label>
-          <input id="username" type="text" className="border-slate-300 border-2" />
+          <input
+            id="username"
+            type="text"
+            className="border-slate-300 border-2 text-black"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" className="border-slate-300 border-2 mb-2" />
-          <Button text="Login" classProps="col-span-2 font-display text-lg"></Button>
+          <input
+            id="password"
+            type="password"
+            className="border-slate-300 border-2 mb-2 text-black"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          <input
+            type="submit"
+            className="col-span-2 font-display text-lg  m-0.5 bg-slate-300 p-0.5 rounded-sm  w-full bg-opacity-10 text-white"
+            value="Log in"
+          />
         </form>
         <Button text="Or Create New Account" classProps="font-display text-lg"></Button>
       </div>
