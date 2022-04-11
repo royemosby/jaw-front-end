eeb5b8a8abdca072f2c5d7f556dd11c4f144c5f6
Roy Mosby <ego.madking@gmail.com>
2022-04-11 12:43:30 -0400

    removes Link inport

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 //import { ContactDetails } from './contacts/ContactDetails'
 import { Contacts } from './contacts/Contacts'
