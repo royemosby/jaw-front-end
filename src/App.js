d3904840eae95a1b3cb3614f79da65a79bbbd63f
Roy Mosby <ego.madking@gmail.com>
2022-04-16 09:16:19 -0400

    stubs open and protected routes

--- src/App.js
+++ src/App.js
@@ -1,42 +1,66 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
-//import { ContactDetails } from './contacts/ContactDetails'
 import { Contacts } from './contacts/Contacts'
+import { NewContact } from './contacts/NewContact'
 import { EditContact } from './contacts/EditContact'
-//import { EditUser } from './user/EditUser'
-//import { EditJob } from './jobs/EditJob'
 import { Footer } from './common/Footer'
 import { Header } from './common/Header'
-//import { JobDetails } from './jobs/JobDetails'
 import { Jobs } from './jobs/Jobs'
-import { Login } from './login/Login'
-import { NewContact } from './contacts/NewContact'
-//import { NewUser } from './user/NewUser'
-//import { NewJob } from './jobs/NewJob'
+import { NewJob } from './jobs/NewJob'
+import { EditJob } from './jobs/EditJob'
+import { Login } from './user/Login'
+import { NewUser } from './user/NewUser'
+import { EditUser } from './user/EditUser'
 import { NotFound } from './common/NotFound'
 import { User } from './user/User'
-
-//import logo from './wrangler.svg'
 import './App.css'
+import { useSelector } from 'react-redux'
 
-const styles = ['min-h-screen', 'grid', 'justify-center', 'text-slate-100', 'content-between', 'p-1'].join(' ')
+const wrapperStyles =
+  'min-h-screen grid justify-center text-slate-100 content-between p-1'
+const mainStyles =
+  'm-auto text-center max-w-6xl	flex flex-col items-stretch h-full border-2 border-slate-600 shadow-cover p-2'
+
+//TODO: fix routes needing :id
 function App() {
+  const user = useSelector((state) => state.user)
+  const protectedRoutes = () => {
+    if (user.jwt) {
+      return (
+        <>
+          <Route index element={<Jobs />} />
+          <Route path="/contacts" element={<Contacts />} />
+          <Route path="/contacts/new" element={<NewContact />} />
+          <Route path="/contacts/edit" element={<EditContact />} />
+          <Route path="/user" element={<User />} />
+          <Route path="/user/edit" element={<EditUser />} />
+          <Route path="/jobs" element={<Jobs />} />
+          <Route path="/jobs/edit" element={<EditJob />} />
+          <Route path="/jobs/new" element={<NewJob />} />
+        </>
+      )
+    } else {
+      return (
+        <>
+          <Route index element={<Login />} />
+        </>
+      )
+    }
+  }
+
   return (
-    <div className={styles}>
+    <div className={wrapperStyles}>
       <Router>
-        <Header className="justify-self-start"></Header>
-        <main className="m-auto text-center max-w-6xl	flex flex-col items-stretch h-full border-2 border-slate-600 shadow-cover p-2">
+        <Header />
+
+        <main className={mainStyles}>
           <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route index element={<Jobs />} />
-            <Route path="/contacts" element={<Contacts />} />
-            <Route path="/user" element={<User />} />
-            <Route path="/x" element={<NewContact />} />
-            <Route path="/y" element={<EditContact />} />
+            {protectedRoutes()}
+            <Route path="/user/new" element={<NewUser />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
         </main>
-        <Footer></Footer>
+        <Footer />
       </Router>
     </div>
   )
