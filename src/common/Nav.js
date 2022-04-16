5bf6f1bceb8d405c69d5ae3f41b72b951cbee5db
Roy Mosby <ego.madking@gmail.com>
2022-04-16 09:20:03 -0400

    hides nav from those not logged on

--- src/common/Nav.js
+++ src/common/Nav.js
@@ -1,28 +1,33 @@
 import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
 import { buttonStyles as styles } from './buttons/Button'
 
 export function Nav() {
-  return (
-    <ul>
-      <Link to="/login" className={styles}>
-        Login
-      </Link>
-      <Link to="/" className={styles}>
-        Jobs
-      </Link>
-      <Link to="/contacts" className={styles}>
-        Contacts
-      </Link>
-      <Link to="/user" className={styles}>
-        User
-      </Link>
-      <Link to="/x" className={styles}>
-        _New Contact
-      </Link>
-      <Link to="/y" className={styles}>
-        _Edit Contact
-      </Link>
-    </ul>
-  )
+  const user = useSelector((state) => state.user)
+  const protectedLinks = () => {
+    if (user.jwt) {
+      return (
+        <>
+          <Link to="/" className={styles}>
+            Jobs
+          </Link>
+          <Link to="/contacts" className={styles}>
+            Contacts
+          </Link>
+          <Link to="/user" className={styles}>
+            User
+          </Link>
+          <Link to="/x" className={styles}>
+            _New Contact
+          </Link>
+          <Link to="/y" className={styles}>
+            _Edit Contact
+          </Link>
+        </>
+      )
+    }
+  }
+
+  return <ul>{protectedLinks()}</ul>
 }
