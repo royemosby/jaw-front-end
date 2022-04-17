3f6416b19e59af5f0fd824451d7fd0b1550552ac
Roy Mosby <ego.madking@gmail.com>
2022-04-17 17:11:02 -0400

    stubs loading jobs state

--- src/jobs/Jobs.js
+++ src/jobs/Jobs.js
@@ -1,15 +1,32 @@
 import { JobCard } from './JobCard'
+import { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { url, getConfig } from '../adapters/config'
+import { addJobs, setJobsMessage } from '../jobs/jobsSlice'
 
 export function Jobs() {
+  const jobs = useSelector((state) => state.jobs.jobs)
+  const message = useSelector((state) => state.jobs.message)
+  const jwt = useSelector((state) => state.user.jwt)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    fetch(url.jobs, getConfig(jwt))
+      .then((resp) => resp.json())
+      .then((resp) => {
+        if (resp.message) {
+          dispatch(setJobsMessage(resp))
+        } else {
+          dispatch(addJobs(resp))
+        }
+      })
+  }, [])
+
+  // const mapJobs = () => {
+  //   return jobs.map((job) => <JobCard job={job} />)
+  // }
   return (
     <div className="">
-      <JobCard></JobCard>
-      <JobCard></JobCard>
-      <JobCard></JobCard>
-      <JobCard></JobCard>
-      <JobCard></JobCard>
-      <JobCard></JobCard>
-      <JobCard></JobCard>
       <JobCard></JobCard>
     </div>
   )
