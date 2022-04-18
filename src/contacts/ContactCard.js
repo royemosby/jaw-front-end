7a16dc7f2ca72a66dc3f683a140919d60fb29d79
Roy Mosby <ego.madking@gmail.com>
2022-04-18 10:56:27 -0400

    adds store content to ContactCard

--- src/contacts/ContactCard.js
+++ src/contacts/ContactCard.js
@@ -1,4 +1,7 @@
 import { Card } from '../common/Card'
+import { useSelector } from 'react-redux'
+import placeholder from '../contact.svg'
+
 import { ConditionalLinkWrapper } from '../common/conditionalLinkWrapper'
 
 export function ContactCard({
@@ -8,31 +11,69 @@ export function ContactCard({
   email,
   url,
   phone,
-  created_at,
+  jobs,
+  id,
   updated_at,
 }) {
+  const storeJobs = useSelector((state) => state.jobs.jobs)
+
+  const contactJobs = () => {
+    if (jobs.length > 0) {
+      const matchingJobs = storeJobs.filter(
+        (j) => j.contact_id === parseInt(id)
+      )
+      return (
+        <div className="border-2 border-slate-500 rounded-sm p-0.5 m-0.5 bg-slate-900/25 max-w-5xl">
+          <h1>Jobs Associated with {first_name}</h1>
+          {matchingJobs.map((j) => (
+            <h2>
+              {j.title} with {j.company} (Status: {j.status})
+            </h2>
+          ))}
+        </div>
+      )
+    }
+  }
+
   return (
     <Card>
-      <p>
-        <ConditionalLinkWrapper link={url} condition={!!url}>
-          {first_name} {last_name}{' '}
-        </ConditionalLinkWrapper>
-        | <a href={`mailto:${email}`}>{email}</a> |{' '}
-        <a href={`tel:${phone}`}>{phone}</a>
-      </p>
+      <div className="flex">
+        <div className="h-16 m-0 5">
+          <img src={placeholder} alt="contact icon" className="h-full" />
+        </div>
+        <div className="text-left grow">
+          <h2 className="text-left">
+            <ConditionalLinkWrapper link={url} condition={!!url}>
+              {first_name} {last_name}{' '}
+            </ConditionalLinkWrapper>
+          </h2>
+          <p>
+            Email: <a href={`mailto:${email}`}>{email}</a> | Phone:{' '}
+            <a href={`tel:${phone}`}>{phone}</a>
+          </p>
+          <p>Type: {contact_type}</p>
+        </div>
+        <div>
+          <p className="text-right">Last updated</p>
+          <p className="text-right">{updated_at.date}</p>
+          <p className="text-right">{updated_at.time}</p>
+        </div>
+      </div>
+
+      {contactJobs()}
     </Card>
   )
 }
 
 /*
-t.string "first_name"
-t.string "last_name"
-t.string "contact_type"
-t.string "email"
-t.string "url"
-t.string "phone"
-t.text "notes"
-t.bigint "user_id", null: false
-t.datetime "created_at", null: false
-t.datetime "updated_at", null: false
+string "first_name"
+string "last_name"
+string "contact_type"
+string "email"
+string "url"
+string "phone"
+text "notes"
+bigint "user_id"
+datetime "created_at"
+obj{date, time} "updated_at"
 */
