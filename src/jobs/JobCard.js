a72b32066afb0ed788e54ae60b3c21adaa7c6e28
Roy Mosby <ego.madking@gmail.com>
2022-04-19 14:35:32 -0400

    loads contacts into store for jobs if not there

--- src/jobs/JobCard.js
+++ src/jobs/JobCard.js
@@ -1,8 +1,11 @@
 import { Card } from '../common/Card'
 import { ConditionalLinkWrapper } from '../common/conditionalLinkWrapper'
 import placeholder from '../honey.svg'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import { useEffect } from 'react'
+import { url, getConfig } from '../adapters/config'
+import { unpad } from '../utilityFunctions/unpad'
+import { addContact, setContactsMessage } from '../contacts/contactsSlice'
 
 export function JobCard({
   company,
@@ -18,10 +21,37 @@ export function JobCard({
   title,
 }) {
   const storeContacts = useSelector((state) => state.contacts.contacts)
+  const storeContactIds = useSelector((state) => state.contacts.contactIds)
+  const storeContactsMessage = useSelector((state) => state.contacts.contactIds)
+  const jwt = useSelector((state) => state.user.jwt)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    if (contact_id && !storeContacts[contact_id]) {
+      fetch(`${url.contacts}/${unpad(contact_id)}`, getConfig(jwt))
+        .then((resp) => resp.json())
+        .then((resp) => {
+          if (resp.message) {
+            dispatch(setContactsMessage(resp))
+          } else {
+            dispatch(addContact(resp))
+          }
+        })
+    }
+  }, [])
 
   const jobContact = () => {
-    if (contact_id) {
-      return <p>There's a contact to deal with yo</p>
+    if (contact_id && storeContacts[contact_id]) {
+      const contact = storeContacts[contact_id]
+      return (
+        <p className="text-left">
+          {contact.first_name} {contact.last_name} |{' '}
+          <a href={`mailto:${contact.email}`}>{contact.email}</a> |{' '}
+          <a href={`tel:$contact.phone`}>{contact.phone}</a>
+        </p>
+      )
+    } else if (contact_id) {
+      return <p className="text-left">Contact not found</p>
     }
   }
   return (
