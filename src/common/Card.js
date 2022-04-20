a45002313240c66b3193e2724030b75f492aab1f
Roy Mosby <ego.madking@gmail.com>
2022-04-20 11:31:19 -0400

    passes open and edit actions to cardButtons

--- src/common/Card.js
+++ src/common/Card.js
@@ -1,11 +1,16 @@
+import { editableInputTypes } from '@testing-library/user-event/dist/utils'
 import { CardButtons } from '../common/buttons/CardButtons'
 
-export function Card({ children }) {
+export function Card({
+  children,
+  openAction = () => console.log('open not wired'),
+  editAction = () => console.log('edit not wired'),
+}) {
   return (
     <div className="border-2 border-slate-500 rounded-sm p-0.5 m-0.5 bg-slate-900/25 w-cover max-w-5xl">
       {children}
 
-      <CardButtons></CardButtons>
+      <CardButtons openAction={openAction} editAction={editAction} />
     </div>
   )
 }
