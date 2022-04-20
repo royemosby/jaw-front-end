c996d720bfa329560ae04ef1862757479e927b97
Roy Mosby <ego.madking@gmail.com>
2022-04-20 11:31:37 -0400

    passes action to button

--- src/common/buttons/Button.js
+++ src/common/buttons/Button.js
@@ -1,15 +1,25 @@
 import { buttonStyles as styles } from './Button'
 
-export function Button({ text, classProps = '' }) {
+export function Button({
+  text,
+  classProps = '',
+  action = () => {
+    console.log('Button not wired')
+  },
+}) {
   const buttonStyles = () => {
     return `${styles} ${classProps}`
   }
 
   return (
-    <button type="button" className={buttonStyles()}>
+    <button
+      type="button"
+      className={buttonStyles()}
+      onClick={(evt) => action(evt)}>
       {text}
     </button>
   )
 }
 
-export const buttonStyles = 'm-0.5 bg-slate-300 p-0.5 rounded-sm  w-full bg-opacity-10 text-white'
+export const buttonStyles =
+  'm-0.5 bg-slate-300 p-0.5 rounded-sm  w-full bg-opacity-10 text-white'
