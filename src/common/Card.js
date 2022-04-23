9e0c8332987cd061921a2b16d682dcc3f41565e5
Roy Mosby <ego.madking@gmail.com>
2022-04-23 18:39:06 -0400

    refine for best practices

--- src/common/Card.js
+++ src/common/Card.js
@@ -1,16 +1,12 @@
 import { editableInputTypes } from '@testing-library/user-event/dist/utils'
 import { CardButtons } from '../common/buttons/CardButtons'
 
-export function Card({
-  children,
-  openAction = () => console.log('open not wired'),
-  editAction = () => console.log('edit not wired'),
-}) {
+export function Card({ children, resourceId = 'notSet', route = 'none' }) {
   return (
     <div className="border-2 border-slate-500 rounded-sm p-0.5 m-0.5 bg-slate-900/25 w-cover max-w-5xl">
       {children}
 
-      <CardButtons openAction={openAction} editAction={editAction} />
+      <CardButtons resourceId={resourceId} route={route} />
     </div>
   )
 }
