381fc37143e4d4b5f1216203926d0840023f8566
Roy Mosby <ego.madking@gmail.com>
2022-04-23 18:41:43 -0400

    stubs functioning buttons

--- src/common/buttons/DetailsButtons.js
+++ src/common/buttons/DetailsButtons.js
@@ -1,11 +1,23 @@
-import { Button } from './Button'
+import { Link } from 'react-router-dom'
+import { buttonStyles as styles } from './Button'
 
-export function DetailsButtons() {
+export function DetailsButtons({ route, resourceId }) {
   return (
-    <div className="flex p-10">
-      <Button text="Edit"></Button>
-      <Button text="Close"></Button>
-      <Button text="Delete"></Button>
+    <div className="flex">
+      <Link
+        className={styles}
+        to={resourceId ? `/${route}/${resourceId}/edit` : '/resource/edit'}>
+        Edit
+      </Link>
+      <Link className={styles} to={resourceId ? `/${route}` : '/resource'}>
+        Close
+      </Link>
+      <Link
+        /*TODO: this is going to have to be a POST resource or trigger a confirmation dialog*/
+        className={styles}
+        to={resourceId ? `/${route}/${resourceId}/delete` : '/resource/delete'}>
+        Delete
+      </Link>
     </div>
   )
 }
