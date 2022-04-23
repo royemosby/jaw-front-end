9d9f40e3a4093c7a7ac5091b7e9289519ec47648
Roy Mosby <ego.madking@gmail.com>
2022-04-23 18:40:35 -0400

    creates links out of buttons

--- src/common/buttons/CardButtons.js
+++ src/common/buttons/CardButtons.js
@@ -1,10 +1,20 @@
 import { Button } from './Button'
+import { Link } from 'react-router-dom'
+import { buttonStyles as styles } from './Button'
 
-export function CardButtons({ openAction, editAction }) {
+export function CardButtons({ route, resourceId }) {
   return (
     <div className="flex">
-      <Button text="Open" action={openAction}></Button>
-      <Button text="Edit" action={editAction}></Button>
+      <Link
+        className={styles}
+        to={resourceId ? `/${route}/${resourceId}` : '/invalid'}>
+        Open
+      </Link>
+      <Link
+        className={styles}
+        to={resourceId ? `/${route}/${resourceId}/edit` : '/invalid/edit'}>
+        Edit
+      </Link>
     </div>
   )
 }
