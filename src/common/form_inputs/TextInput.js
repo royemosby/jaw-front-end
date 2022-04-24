2315e9a8ef46d7d9169ad9195bd89084fb19c080
Roy Mosby <ego.madking@gmail.com>
2022-04-24 06:26:14 -0400

    adds in additional expected props

--- src/common/form_inputs/TextInput.js
+++ src/common/form_inputs/TextInput.js
@@ -1,3 +1,19 @@
-export function TextInput({ name, id, required = false, placeholder = '' }) {
-  return <input type="text" id={id} name={name} className="text-black" required={required} placeholder={placeholder} />
+export function TextInput({
+  name,
+  id,
+  required = false,
+  placeholder = '',
+  value,
+}) {
+  return (
+    <input
+      type="text"
+      id={id}
+      name={name}
+      className="text-black"
+      required={required}
+      placeholder={placeholder}
+      value={value}
+    />
+  )
 }
