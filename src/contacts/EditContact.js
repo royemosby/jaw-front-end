a3f19f2451e61a34e78a2604b4b7fe3a8ee1009a
Roy Mosby <ego.madking@gmail.com>
2022-04-24 06:31:49 -0400

    stubs edit actions for contact form

--- src/contacts/EditContact.js
+++ src/contacts/EditContact.js
@@ -1,12 +1,30 @@
 import { ContactForm } from './ContactForm'
 import { UpdateButtons } from '../common/buttons/UpdateButtons'
+import { useNavigate } from 'react-router-dom'
 
 export function EditContact() {
+  const navigate = useNavigate()
+
+  const handleCancel = () => {
+    return navigate(-1)
+  }
+
+  const handleSubmit = () => {
+    console.log('contact submitted')
+  }
+  const handleDelete = () => {
+    console.log('contact deleted')
+  }
   return (
     <div>
       <h1>Edit Contact</h1>
-      <ContactForm />
-      <UpdateButtons />
+      <ContactForm>
+        <UpdateButtons
+          handleCancel={handleCancel}
+          handleDelete={handleDelete}
+          handleSubmit={handleSubmit}
+        />
+      </ContactForm>
     </div>
   )
 }
