0d5b4fe3d3c296c1e1a016ffcc7673a0bf3b892c
Roy Mosby <ego.madking@gmail.com>
2022-04-24 06:31:32 -0400

    stubs edit actions for job form

--- src/jobs/EditJob.js
+++ src/jobs/EditJob.js
@@ -1,12 +1,35 @@
 import { JobForm } from './JobForm'
 import { UpdateButtons } from '../common/buttons/UpdateButtons'
+import { useParams } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 
 export function EditJob() {
+  const { jobId } = useParams()
+  const job = useSelector((state) => state.jobs.jobs[jobId])
+  const navigate = useNavigate()
+
+  const handleCancel = () => {
+    return navigate(-1)
+  }
+
+  const handleSubmit = () => {
+    console.log('job submitted')
+  }
+  const handleDelete = () => {
+    console.log('job deleted')
+  }
+
   return (
     <div>
       <h1>Edit Job</h1>
-      <JobForm></JobForm>
-      <UpdateButtons></UpdateButtons>
+      <JobForm job={job}>
+        <UpdateButtons
+          handleCancel={handleCancel}
+          handleDelete={handleDelete}
+          handleSubmit={handleSubmit}
+        />
+      </JobForm>
     </div>
   )
 }
