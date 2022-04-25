292a8cb9ef0f73892bdc29bb73fb63cbcc8e06db
Roy Mosby <ego.madking@gmail.com>
2022-04-25 08:26:06 -0400

    adds delete dialog stub

--- src/contacts/EditContact.js
+++ src/contacts/EditContact.js
@@ -1,30 +1,52 @@
 import { ContactForm } from './ContactForm'
 import { UpdateButtons } from '../common/buttons/UpdateButtons'
 import { useNavigate } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { DeleteDialog } from '../common/DeleteDialog'
+import { useState } from 'react'
 
 export function EditContact() {
+  const [dialogIsOpen, setDialogIsOpen] = useState(false)
+  const { contactId } = useParams()
+  const contact = useSelector((state) => state.contacts.contacts[contactId])
   const navigate = useNavigate()
 
   const handleCancel = () => {
     return navigate(-1)
+  }
+  const handleDialogCancel = () => {
+    setDialogIsOpen(false)
+  }
+  const handleDialogConfirm = () => {
+    alert('Confirm delete pressed')
+    setDialogIsOpen(false)
   }
 
   const handleSubmit = () => {
     console.log('contact submitted')
   }
   const handleDelete = () => {
-    console.log('contact deleted')
+    setDialogIsOpen(true)
   }
   return (
-    <div>
+    <div className="relative">
       <h1>Edit Contact</h1>
-      <ContactForm>
+      <ContactForm contact={contact}>
         <UpdateButtons
           handleCancel={handleCancel}
           handleDelete={handleDelete}
           handleSubmit={handleSubmit}
         />
       </ContactForm>
+      {dialogIsOpen ? (
+        <DeleteDialog
+          title="Confirm Contact Delete"
+          text="This action may not currently work if there are jobs associated with this contact. Removing contact's job associations is on the todo"
+          cancelHandler={handleDialogCancel}
+          confirmHandler={handleDialogConfirm}
+          isOpen={dialogIsOpen}></DeleteDialog>
+      ) : null}
     </div>
   )
 }
