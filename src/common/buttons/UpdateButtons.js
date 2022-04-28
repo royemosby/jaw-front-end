10804f1abd087adfcc529dbb4ba64cb790264606
Roy Mosby <ego.madking@gmail.com>
2022-04-28 16:16:07 -0400

    replaces submit button with input type submit

--- src/common/buttons/UpdateButtons.js
+++ src/common/buttons/UpdateButtons.js
@@ -1,10 +1,11 @@
 import { Button } from './Button'
+import { InputSubmit } from './InputSubmit'
 
 //TODO: rename to editFormButtons
 export function UpdateButtons({ handleSubmit, handleDelete, handleCancel }) {
   return (
     <div className="flex col-span-2">
-      <Button text="Update" action={handleSubmit}></Button>
+      <InputSubmit submit={handleSubmit} text="Update"></InputSubmit>
       <Button text="Cancel" action={handleCancel}></Button>
       <Button text="Delete" action={handleDelete}></Button>
     </div>
