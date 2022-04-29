51342e35c6bcba0ea49e954b85b6485105270138
Roy Mosby <ego.madking@gmail.com>
2022-04-29 12:01:42 -0400

    adds updating functionality

--- src/contacts/EditContact.js
+++ src/contacts/EditContact.js
@@ -1,42 +1,72 @@
 import { ContactForm } from './ContactForm'
 import { UpdateButtons } from '../common/buttons/UpdateButtons'
-import { useNavigate } from 'react-router-dom'
-import { useParams } from 'react-router-dom'
-import { useSelector } from 'react-redux'
+import { useNavigate, useParams } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
 import { DeleteDialog } from '../common/DeleteDialog'
 import { useState } from 'react'
-import { deleteConfig, putConfig } from '../adapters/config'
+import { url, deleteConfig, putConfig } from '../adapters/config'
+import { updateContact, setContactsMessage } from './contactsSlice'
 
 export function EditContact() {
   const [dialogIsOpen, setDialogIsOpen] = useState(false)
   const { contactId } = useParams()
+  const jwt = useSelector((state) => state.user.jwt)
   const contact = useSelector((state) => state.contacts.contacts[contactId])
   const navigate = useNavigate()
+  const dispatch = useDispatch()
 
+  const disableDelete = () => {
+    if (contact.jobIds.length === 0) {
+      return false
+    } else {
+      return true
+    }
+  }
+
+  const handleSubmit = ({ event, submittedContact }) => {
+    event.preventDefault()
+    fetch(`${url.contacts}/${contact.id}`, putConfig(jwt, submittedContact))
+      .then((resp) => resp.json())
+      .then((resp) => {
+        if (resp.message) {
+          dispatch(setContactsMessage(resp))
+        } else {
+          dispatch(updateContact(resp))
+          navigate(-1)
+        }
+      })
+  }
   const handleCancel = () => {
     return navigate(-1)
-  }
-  const handleDialogCancel = () => {
-    setDialogIsOpen(false)
-  }
-  const handleDialogConfirm = () => {
-    alert('Confirm delete pressed')
-    setDialogIsOpen(false)
-  }
-
-  const handleSubmit = ({ event, newContact }) => {
-    console.log(JSON.stringify(newContact))
-    event.preventDefault()
   }
   const handleDelete = () => {
     setDialogIsOpen(true)
   }
+
+  const handleDialogCancel = () => {
+    setDialogIsOpen(false)
+  }
+  const handleDialogConfirm = () => {
+    fetch(`${url.contacts}/${contact.id}`, deleteConfig(jwt))
+      .then((resp) => {
+        console.log(resp.ok, resp.headers)
+        return resp.json()
+      })
+      .then((resp) => {
+        if (resp.message) {
+          dispatch(setContactsMessage(resp))
+        }
+        navigate('/contacts')
+      })
+    setDialogIsOpen(false)
+  }
+
   return (
     <div className="relative">
       <h1>Edit Contact</h1>
       <ContactForm contact={contact} handleSubmit={handleSubmit}>
         <UpdateButtons
-          handleSubmit={handleSubmit}
+          disabled={disableDelete()}
           handleCancel={handleCancel}
           handleDelete={handleDelete}
         />
