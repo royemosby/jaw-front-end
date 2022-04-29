078aa2707e47c95e78e17be2459db018df7820cc
Roy Mosby <ego.madking@gmail.com>
2022-04-29 11:59:48 -0400

    expands button options. adds delete confirmation dialog

--- src/contacts/ContactDetails.js
+++ src/contacts/ContactDetails.js
@@ -1,9 +1,15 @@
 import { DetailsButtons } from '../common/buttons/DetailsButtons'
-import { useParams } from 'react-router-dom'
-import { useSelector } from 'react-redux'
+import { useParams, useNavigate } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
 import { JobCardFragment } from './jobCardFragment'
+import { DeleteDialog } from '../common/DeleteDialog'
+import { useState } from 'react'
+//url name collision w/contact.url
+import { url as deleteUrl, deleteConfig } from '../adapters/config'
+import { setContactsMessage } from './contactsSlice'
 
 export function ContactDetails() {
+  const [dialogIsOpen, setDialogIsOpen] = useState(false)
   const { contactId } = useParams()
   const {
     first_name,
@@ -15,7 +21,43 @@ export function ContactDetails() {
     url,
     updated_at,
     jobIds,
+    id,
   } = useSelector((state) => state.contacts.contacts[contactId])
+
+  const disableDelete = () => {
+    if (jobIds.length === 0) {
+      return false
+    } else {
+      return true
+    }
+  }
+
+  const jwt = useSelector((state) => state.user.jwt)
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  const handleDelete = () => {
+    setDialogIsOpen(true)
+  }
+  const handleDialogCancel = () => {
+    setDialogIsOpen(false)
+  }
+  const handleDialogConfirm = () => {
+    fetch(`${deleteUrl.contacts}/${id}`, deleteConfig(jwt))
+      .then((resp) => {
+        if (resp.ok) {
+          return resp.json()
+        } else {
+          console.log('deletion failed')
+          console.dir(resp.headers)
+        }
+      })
+      .then((resp) => {
+        dispatch(setContactsMessage(resp))
+        navigate('/contacts')
+      })
+    setDialogIsOpen(false)
+  }
   return (
     <div className="bg-slate-900/25 min-h-cover">
       <div className="text-left p-2 shadow-cover-divider bg-slate-900/50 flex justify-between	">
@@ -56,20 +98,20 @@ export function ContactDetails() {
           <div>{notes}</div>
         </details>
       </div>
-      <DetailsButtons route="contacts" resourceId={contactId} />
+      <DetailsButtons
+        route="contacts"
+        resourceId={contactId}
+        handleDelete={() => handleDelete()}
+        disabled={disableDelete()}
+      />
+      {dialogIsOpen ? (
+        <DeleteDialog
+          title="Confirm Contact Delete"
+          text="This action may not currently work if there are jobs associated with this contact. Removing contact's job associations is on the todo"
+          cancelHandler={handleDialogCancel}
+          confirmHandler={handleDialogConfirm}
+          isOpen={dialogIsOpen}></DeleteDialog>
+      ) : null}
     </div>
   )
 }
-
-/*
-t.string "first_name"
-t.string "last_name"
-t.string "contact_type"
-t.string "email"
-t.string "url"
-t.string "phone"
-t.text "notes"
-t.bigint "user_id", null: false
-t.datetime "created_at", null: false
-t.datetime "updated_at", null: false
-*/
