58f868b51587d32fc619dd0cdd65dfe3fd21071f
Roy Mosby <ego.madking@gmail.com>
2022-04-29 11:58:10 -0400

    removes handleSubmit and adds props to disable delete button

--- src/common/buttons/UpdateButtons.js
+++ src/common/buttons/UpdateButtons.js
@@ -2,12 +2,16 @@ import { Button } from './Button'
 import { InputSubmit } from './InputSubmit'
 
 //TODO: rename to editFormButtons
-export function UpdateButtons({ handleSubmit, handleDelete, handleCancel }) {
+export function UpdateButtons({
+  handleDelete,
+  handleCancel,
+  disabled = false,
+}) {
   return (
     <div className="flex col-span-2">
-      <InputSubmit submit={handleSubmit} text="Update"></InputSubmit>
+      <InputSubmit text="Update"></InputSubmit>
       <Button text="Cancel" action={handleCancel}></Button>
-      <Button text="Delete" action={handleDelete}></Button>
+      <Button text="Delete" disabled={disabled} action={handleDelete}></Button>
     </div>
   )
 }
