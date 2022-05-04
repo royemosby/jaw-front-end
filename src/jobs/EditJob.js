7d71f75db9bf2f7c78cbe3f5364f30a421017b55
Roy Mosby <ego.madking@gmail.com>
2022-05-04 09:29:44 -0400

    wires form to put/delete

--- src/jobs/EditJob.js
+++ src/jobs/EditJob.js
@@ -1,35 +1,94 @@
 import { JobForm } from './JobForm'
 import { UpdateButtons } from '../common/buttons/UpdateButtons'
 import { useParams } from 'react-router-dom'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
+import { url, putConfig, deleteConfig } from '../adapters/config'
+import { useState } from 'react'
+import { DeleteDialog } from '../common/DeleteDialog'
+import { updateJob, deleteJob, setJobsMessage } from './jobsSlice'
 
 export function EditJob() {
   const { jobId } = useParams()
   const job = useSelector((state) => state.jobs.jobs[jobId])
+  const jwt = useSelector((state) => state.user.jwt)
   const navigate = useNavigate()
+  const dispatch = useDispatch()
+  const [dialogIsOpen, setDialogIsOpen] = useState(false)
 
   const handleCancel = () => {
     return navigate(-1)
   }
 
-  const handleSubmit = () => {
-    console.log('job submitted')
+  const handleSubmit = ({ event, submittedJob }) => {
+    event.preventDefault()
+    fetch(`${url.jobs}/${job.id}`, putConfig(jwt, submittedJob))
+      .then((resp) => {
+        return resp.json()
+      })
+      .then((resp) => {
+        if (resp.message) {
+          console.log(resp.message)
+          dispatch(setJobsMessage(resp.message))
+        } else {
+          dispatch(updateJob(resp))
+          navigate(-1)
+        }
+      })
   }
+
+  const disableDelete = () => {
+    if (job.contact_id) {
+      return true
+    } else {
+      return false
+    }
+  }
+
   const handleDelete = () => {
-    console.log('job deleted')
+    setDialogIsOpen(true)
+  }
+
+  const handleDialogCancel = () => {
+    setDialogIsOpen(false)
+  }
+
+  const handleDialogConfirm = () => {
+    fetch(`${url.jobs}/${job.id}`, deleteConfig(jwt))
+      .then((resp) => {
+        if (resp.ok) {
+          return resp.json()
+        } else {
+          console.log('deletion failed')
+          console.dir(resp.headers)
+        }
+      })
+      .then((resp) => {
+        setDialogIsOpen(false)
+        navigate('/jobs')
+        dispatch(setJobsMessage(resp))
+        dispatch(deleteJob(jobId))
+      })
   }
 
   return (
     <div>
       <h1>Edit Job</h1>
-      <JobForm job={job}>
+      <JobForm handleSubmit={handleSubmit} job={job}>
         <UpdateButtons
+          disabled={disableDelete()}
           handleCancel={handleCancel}
           handleDelete={handleDelete}
-          handleSubmit={handleSubmit}
         />
       </JobForm>
+      {dialogIsOpen ? (
+        <DeleteDialog
+          title="Confirm Job Delete"
+          text="This action may not currently work if there are contacts associated with this job. Removing contact's job associations is on the todo"
+          cancelHandler={handleDialogCancel}
+          confirmHandler={handleDialogConfirm}
+          isOpen={dialogIsOpen}></DeleteDialog>
+      ) : null}
     </div>
   )
 }
