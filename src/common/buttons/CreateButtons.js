09416d9091963aeea7c3affcb18bf90b86fa81d5
Roy Mosby <ego.madking@gmail.com>
2022-05-04 09:29:07 -0400

    corrects buttons layout in grid

--- src/common/buttons/CreateButtons.js
+++ src/common/buttons/CreateButtons.js
@@ -2,7 +2,7 @@ import { Button } from './Button'
 
 export function CreateButtons() {
   return (
-    <div className="flex">
+    <div className=" col-span-2 flex">
       <Button text="Create"></Button>
       <Button text="Cancel"></Button>
     </div>
