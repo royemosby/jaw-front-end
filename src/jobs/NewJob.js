480a990dad809ee75a6c5eedca2b7817136e7c20
Roy Mosby <ego.madking@gmail.com>
2022-05-05 11:58:27 -0400

    adds post, cancel actions

--- src/jobs/NewJob.js
+++ src/jobs/NewJob.js
@@ -1,12 +1,38 @@
 import { JobForm } from './JobForm'
 import { CreateButtons } from '../common/buttons/CreateButtons'
+import { addJob, setJobsMessage } from './jobsSlice'
+import { useNavigate } from 'react-router-dom'
+import { url, postConfig } from '../adapters/config'
+import { useSelector, useDispatch } from 'react-redux'
+import { zeroPad } from '../utilityFunctions/zeroPad'
 
 export function NewJob() {
+  const navigate = useNavigate()
+  const jwt = useSelector((state) => state.user.jwt)
+  const dispatch = useDispatch()
+
+  const handleSubmit = ({ event, job }) => {
+    event.preventDefault()
+    fetch(url.jobs, postConfig(jwt, job))
+      .then((resp) => resp.json())
+      .then((resp) => {
+        if (resp.message) {
+          setJobsMessage(resp)
+        } else {
+          dispatch(addJob(resp))
+          navigate(`/jobs/job${zeroPad(resp.data.attributes.id)}`)
+        }
+      })
+  }
+  const handleCancel = () => navigate(-1)
+
   return (
     <div>
       <h1>New Job</h1>
-      <JobForm>
-        <CreateButtons></CreateButtons>
+      <JobForm handleSubmit={handleSubmit}>
+        <CreateButtons
+          handleCancel={handleCancel}
+          inputText="Create Job"></CreateButtons>
       </JobForm>
     </div>
   )
