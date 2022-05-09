ad516b7c052ff6bf3d94ec83fed51fd2c06f7568
Roy Mosby <ego.madking@gmail.com>
2022-05-09 10:56:04 -0400

    init commit

---
+++ src/common/ModalShell.js
@@ -0,0 +1,13 @@
+export function ModalShell({ children }) {
+  const handleSubmit = (e) => {
+    console.log(e.target.value)
+  }
+  return (
+    <>
+      <div className="bg-slate-400/50 h-screen w-screen absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 backdrop-blur-sm"></div>
+      <div className="bg-slate-900 text-white absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 p-4 min-w-full">
+        {children}
+      </div>
+    </>
+  )
+}
