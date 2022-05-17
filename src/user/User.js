7a67aceb0ac1af402804a268437c77a077f5ffaa
Roy Mosby <ego.madking@gmail.com>
2022-05-17 09:34:08 -0400

    adds job counts by status

--- src/user/User.js
+++ src/user/User.js
@@ -1,37 +1,53 @@
+import { useSelector } from 'react-redux'
 import { Button } from '../common/buttons/Button'
 
 export function User() {
+  const user = useSelector((state) => state.user)
+  const jobs = useSelector((state) => state.jobs.jobs)
+
+  const jobsArray = Object.keys(jobs).map((key) => jobs[key])
+  const newJobs = jobsArray.filter((job) => job.status === 'new')
+  const appliedJobs = jobsArray.filter((job) => job.status === 'applied')
+  const interviewingJobs = jobsArray.filter(
+    (job) => job.status === 'interviewing'
+  )
+  const offerJobs = jobsArray.filter((job) => job.status === 'offer')
+  const acceptedJobs = jobsArray.filter((job) => job.status === 'accepted')
+  const rejectedJobs = jobsArray.filter((job) => job.status === 'rejected')
+  const declinedJobs = jobsArray.filter((job) => job.status === 'declined')
+  const closedJobs = jobsArray.filter((job) => job.status === 'closed')
+
   return (
     <div className=" w-cover min-h-cover max-w-5xl">
       <header className="bg-slate-900/50 shadow-cover-divider p-1 mb-2">
         <h2>Account info</h2>
-        <h3>Username: royemosby</h3>
-        <h3>Email: roy.e.mosby@gmail.com</h3>
+        <h3>Username: {user.username}</h3>
+        <h3>Email: {user.email || 'none provided'}</h3>
       </header>
       <div>
         <h2>Jobs summaries</h2>
         <div className="grid grid-cols-2 content-center max-w-xs m-auto shadow-cover-divider p-1 mb-2">
-          <div>Total</div>
-          <div>42</div>
           <div>New</div>
-          <div>8</div>
+          <div>{newJobs.length}</div>
           <div>Applied</div>
-          <div>4</div>
+          <div>{appliedJobs.length}</div>
           <div>Interviewing</div>
-          <div>2</div>
+          <div>{interviewingJobs.length}</div>
           <div>Offer</div>
-          <div>3</div>
+          <div>{offerJobs.length}</div>
           <div>Accepted</div>
-          <div>0</div>
+          <div>{acceptedJobs.length}</div>
           <div>Rejected</div>
-          <div>4</div>
+          <div>{rejectedJobs.length}</div>
           <div>Declined</div>
-          <div>5</div>
+          <div>{declinedJobs.length}</div>
           <div>Closed</div>
-          <div>9</div>
+          <div>{closedJobs.length}</div>
+          <hr /> <hr />
+          <div>Total</div>
+          <div>{jobsArray.length}</div>
         </div>
       </div>
-      <div>Perf dash???</div>
       <div className="flex">
         <Button text="Upate Email"></Button>
         <Button text="Upate Password"></Button>
